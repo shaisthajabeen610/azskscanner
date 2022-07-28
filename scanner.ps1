8f00a6f35924033372cc89c87b1a1541e8b3d575
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-28 11:49:43 +0530

    commit3

--- scanner.ps1
+++ scanner.ps1
@@ -1,5 +1,7 @@
 ﻿   Install-Module Az -Force
 Import-Module Az   
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
  Connect-AzAccount 
  $Id = Get-Azsubscription
  
