9d46d875f62a34855efd7ef0781be56244ff303b
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-29 09:58:39 +0530

    Update subscriptionscanner.ps1

--- subscriptionscanner.ps1
+++ subscriptionscanner.ps1
@@ -67,7 +67,7 @@ if ($env:PATH -notcontains $InstallPath) {
  {
  $draft = Get-AzSKSubscriptionSecurityStatus -SubscriptionId $Ids.id
 $children = Get-ChildItem -Filter *.csv $draft
-azcopy copy $draft/$children "https://azsk1.blob.core.windows.net/azsk/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-07-28T21:07:10Z&st=2022-07-28T13:07:10Z&spr=https&sig=Aw41bTLEimlGyw3rgyi7JXnijAijpnZk4sXBCZmCnGQ%3D" --recursive=true
+azcopy copy $draft/$children "https://azsk1.blob.core.windows.net/azsk/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-08-17T12:26:49Z&st=2022-07-29T04:26:49Z&spr=https,http&sig=aZTMemnP%2FCmT%2BBUhG52hc6QTEEoH3zGSw1qvK8oUOZo%3D" --recursive=true
  
  }
 
