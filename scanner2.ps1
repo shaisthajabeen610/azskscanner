3fd26dba4ee1f8c3214b928fe926c82c279901c3
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-29 10:42:32 +0530

    Update scanner2.ps1

--- scanner2.ps1
+++ scanner2.ps1
@@ -1,4 +1,5 @@
   Install-Module Az -AllowClobber -Force 
 Import-Module Az   
 Install-Module -Name AzSK -Scope CurrentUser -AllowClobber -Force -SkipPublisherCheck
+Set-AzSKPolicySettings -OnlinePolicyStoreUrl "https://raw.githubusercontent.com/azsk/DevOpsKit/master/src/oss-config/`$Version/`$FileName"
 Import-Module AzSK 
