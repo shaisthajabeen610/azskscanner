4ae2aff26ada6776370d178fc087add728ebb05e
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-08-17 16:05:39 +0530

    Update scanner.ps1

--- scanner.ps1
+++ scanner.ps1
@@ -9,13 +9,14 @@ Connect-AzAccount -UseDeviceAuthentication
 # Install-Module AzSK -Scope CurrentUser -SkipPublisherCheck  -Force
 # Import-Module AzSK 
 Install-Module -Name AzSK -Scope CurrentUser -AllowClobber -Force 
-Import-Module AzSK 
-Set-AzSKPolicySettings -AutoUpdate On
- foreach ($Ids in $Id)
- {
- $draft = Get-AzSKSubscriptionSecurityStatus -SubscriptionId $Ids.id
+set-executionpolicy RemoteSigned -Scope Process -Force;
+Import-Module AzSK;
+Connect-AzAccount 
+Set-AzSKPrivacyNoticeResponse -AcceptPrivacyNotice "yes";
+$Id = Get-Azsubscription;
+foreach ($Ids in $Id)
+{
+$draft = Get-AzSKSubscriptionSecurityStatus -SubscriptionId $Ids.id
 $children = Get-ChildItem -Filter *.csv $draft
-azcopy copy "$draft/$children" "https://azsk1.blob.core.windows.net/azsk/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-07-27T16:15:43Z&st=2022-07-27T08:15:43Z&spr=https,http&sig=8MXZcgy3yJBbaQGWTCME4ZpAOPFxBPB7OVUmjgFtPCI%3D" --recursive=true
- Remove-Item $draft
- }
- 
+C:\source\azcopy.exe copy "$draft/$children" "https://azsk1.blob.core.windows.net/azsk/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-08-25T15:30:48Z&st=2022-08-17T07:30:48Z&spr=https,http&sig=%2BVQcN%2F0WSg%2F1Xy9xmba4OYTHJooTJeW9%2FpI0Ttjxuog%3D" --recursive=true
+}
